593b4a27e34611bcdd77b8513c76c3a95413d862
TEJA_1290 <39546736+saiteja1290@users.noreply.github.com>
2022-05-25 19:39:34 +0530

    took out set inetval as forward

--- app.js
+++ app.js
@@ -9,13 +9,34 @@ let backward = document.getElementById("one");
 let pause = document.getElementById("four");
 
 let songs = [
-  { songname: "Kaikai Kitan (JJK) - Kijugo", filepath: "/VIBHA/songs/Kaikai Kitan (JJK) - Kijugo.mp3" },
-  { songname: "Kiss Of Death (Darling In The Franxx) - Kijuro", filepath: "/VIBHA/songs/Kiss Of Death (Darling In The Franxx) - Kijuro.mp3" },
-  { songname: "Lost In Paradise (JJK) - Kijugo", filepath: "/VIBHA/songs/Lost In Paradise (JJK) - Kijugo.mp3" },
-  { songname: "My War (AOT) - Kijugo", filepath: "/VIBHA/songs/My War (AOT) - Kijugo.mp3" },
-  { songname: "Shinzo Wo Sasageyo (AOT) - Kijuro", filepath: "/VIBHA/songs/Shinzo Wo Sasageyo (AOT) - Kijuro.mp3" },
-  { songname: "Unravel (Tokyo Ghoul) - Kijugo", filepath: "/VIBHA/songs/Unravel (Tokyo Ghoul) - Kijugo.mp3" },
-  { songname: "kataware doki (Your Name) - Kijugo", filepath: "/VIBHA/songs/kataware doki (Your Name) - Kijugo.mp3" },
+  {
+    songname: "Kaikai Kitan (JJK) - Kijugo",
+    filepath: "/songs/Kaikai Kitan (JJK) - Kijugo.mp3",
+  },
+  {
+    songname: "Kiss Of Death (Darling In The Franxx) - Kijuro",
+    filepath: "/songs/Kiss Of Death (Darling In The Franxx) - Kijuro.mp3",
+  },
+  {
+    songname: "Lost In Paradise (JJK) - Kijugo",
+    filepath: "/songs/Lost In Paradise (JJK) - Kijugo.mp3",
+  },
+  {
+    songname: "My War (AOT) - Kijugo",
+    filepath: "/songs/My War (AOT) - Kijugo.mp3",
+  },
+  {
+    songname: "Shinzo Wo Sasageyo (AOT) - Kijuro",
+    filepath: "/songs/Shinzo Wo Sasageyo (AOT) - Kijuro.mp3",
+  },
+  {
+    songname: "Unravel (Tokyo Ghoul) - Kijugo",
+    filepath: "/songs/Unravel (Tokyo Ghoul) - Kijugo.mp3",
+  },
+  {
+    songname: "kataware doki (Your Name) - Kijugo",
+    filepath: "/songs/kataware doki (Your Name) - Kijugo.mp3",
+  },
 ];
 let songindex = 0;
 // randomnumber = Math.floor((Math.random() * 5));
@@ -33,33 +54,34 @@ play.addEventListener("click", () => {
     play.classList.add("fa-play");
     mastersongname.innerHTML = songs[songindex].songname;
   }
-  setInterval(()=>{
-    if (songindex >= 4) {
-      songindex = 0;
-    } else {
-      songindex++;
-    }
-    audioelement = new Audio(songs[songindex].filepath);
-  if (audioelement.paused || audioelement.currentTime == 0) {
-    audioelement.play();
-    play.classList.remove("fa-play");
-    play.classList.add("fa-pause");
-    mastersongname.innerHTML = songs[songindex].songname;
-  } else {
-    audioelement.pause();
-    play.classList.remove("fa-pause");
-    play.classList.add("fa-play");
-    mastersongname.innerHTML = songs[songindex].songname;
-  }
-  audioelement.addEventListener('timeupdate',()=>{
-    howla = parseInt((audioelement.currentTime / audioelement.duration) * 100);
-    myprogbar.value = howla;
-  })
-  }, audioelement.duration * 1000);
+
+  // setInterval(()=>{
+  //   if (songindex >= 4) {
+  //     songindex = 0;
+  //   } else {
+  //     songindex++;
+  //   }
+  //   audioelement = new Audio(songs[songindex].filepath);
+  // if (audioelement.paused || audioelement.currentTime == 0) {
+  //   audioelement.play();
+  //   play.classList.remove("fa-play");
+  //   play.classList.add("fa-pause");
+  //   mastersongname.innerHTML = songs[songindex].songname;
+  // } else {
+  //   audioelement.pause();
+  //   play.classList.remove("fa-pause");
+  //   play.classList.add("fa-play");
+  //   mastersongname.innerHTML = songs[songindex].songname;
+  // }
+  // audioelement.addEventListener('timeupdate',()=>{
+  //   howla = parseInt((audioelement.currentTime / audioelement.duration) * 100);
+  //   myprogbar.value = howla;
+  // })
+  // }, audioelement.duration * 1000);
 });
 
-if (audioelement.currentTime == audioelement.duration) {
-}
+// if (audioelement.currentTime == audioelement.duration) {
+// }
 forward.addEventListener("click", () => {
   // Forward
   audioelement.pause();
